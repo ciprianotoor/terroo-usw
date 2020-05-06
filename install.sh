b3526a7150256864bb1d7d849598acd13950d488
Marcos Oliveira <marcos@terminalroot.com.br>
2020-05-06 14:20:12 -0300

    add initial files

---
+++ install.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+# script: install.sh
+# description: Customize your Ubuntu with Wayland + Sway + Ulauncher + Waybar
+# author: Marcos Oliveira <terminalroot.com.br>
+# license: GPLv3
+# dependences: git
+
+_start(){
+  read -n1 -p "Do you like start installation? [Y/n]: "
+  [[ "$REPLY" == "y" ||  "$REPLY" == "Y" ]] && {
+    printf "\n%s\n" "\o/"
+  } || {
+    echo
+    exit 0
+  }
+}
+
+_install_pkgs(){
+  sudo apt install sway waybar nvim mpd mpc ncmpcpp
+  sudo add-apt-repository ppa:agornostal/ulauncher
+  sudo apt update
+  sudo apt install ulauncher
+  sudo add-apt-repository --remove ppa:agornostal/ulauncher
+  sudo apt update
+}
+
+_configs_pkgs(){
+  mv sway/ ${HOME}/.config/
+  sed -i "s/__USER__/$USER/" ${HOME}/.config/sway/config
+  mv ulauncher/ ${HOME}/.config/
+  mv wallpaper.jpg ${HOME}/.wallpaper.jpg
+  mv waybar/ ${HOME}/.config/
+  chmod +x ${HOME}/.config/waybar/waybar.sh
+  mv fonts/ ${HOME}/.local/share/
+  fc-cache -fv
+  source ${HOME}/.bashrc
+}
+
+
+_msg_final(){
+    clear
+    printf "%s\n" "
+    ███████╗██╗   ██╗ ██████╗ ██████╗███████╗███████╗███████╗██╗
+    ██╔════╝██║   ██║██╔════╝██╔════╝██╔════╝██╔════╝██╔════╝██║
+    ███████╗██║   ██║██║     ██║     █████╗  ███████╗███████╗██║
+    ╚════██║██║   ██║██║     ██║     ██╔══╝  ╚════██║╚════██║╚═╝
+    ███████║╚██████╔╝╚██████╗╚██████╗███████╗███████║███████║██╗
+    ╚══════╝ ╚═════╝  ╚═════╝ ╚═════╝╚══════╝╚══════╝╚══════╝╚═╝
+                END THE SESSION AND ENJOY!
+    "
+}
+
+_start
+#_install_pkgs
+#_configs_pkgs
+_msg_final
+exit 0
