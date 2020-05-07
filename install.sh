f0b229c4933000b5dce657da03a0455d01adb663
Marcos Oliveira <marcos@terminalroot.com.br>
2020-05-06 21:52:35 -0300

    Making the script faster

--- install.sh
+++ install.sh
@@ -1,9 +1,19 @@
 #!/usr/bin/env bash
+# vim: noai:ts=4:sw=4:expandtab
+################################################################################
 # script: install.sh
 # description: Customize your Ubuntu with Wayland + Sway + Ulauncher + Waybar
 # author: Marcos Oliveira <terminalroot.com.br>
 # license: GPLv3
 # dependences: git
+################################################################################
+
+export _pkgs=(sway waybar vim mpd mpc ncmpcpp ulauncher)
+export _conf="${HOME}/.config/"
+export _lsf="${HOME}/.local/share"
+export _wall="wallpaper.jpg"
+export _sa="sudo apt"
+export _oth="fonts fc-cache bashrc"
 
 _start(){
   read -n1 -p "Do you like start installation? [Y/n]: "
@@ -16,24 +26,23 @@ _start(){
 }
 
 _install_pkgs(){
-  sudo apt install sway waybar vim mpd mpc ncmpcpp -y
-  sudo add-apt-repository -y ppa:agornostal/ulauncher
-  sudo apt update
-  sudo apt install ulauncher -y
-  sudo add-apt-repository --remove -y ppa:agornostal/ulauncher
-  sudo apt update
+  ${_sa}-apt-repository -y ppa:agornostal/${_pkgs[6]}
+  ${_sa} update
+  ${_sa} install $(printf '%s ' "${_pkgs[@]}") -y
+  ${_sa}-apt-repository --remove -y ppa:agornostal/${_pkgs[6]}
+  ${_sa} update
 }
 
 _configs_pkgs(){
-  mv sway/ ${HOME}/.config/
-  sed -i "s/__USER__/$USER/" ${HOME}/.config/sway/config
-  mv ulauncher/ ${HOME}/.config/
-  mv wallpaper.jpg ${HOME}/.wallpaper.jpg
-  mv waybar/ ${HOME}/.config/
-  chmod +x ${HOME}/.config/waybar/waybar.sh
-  mv fonts/ ${HOME}/.local/share/
-  fc-cache -fv
-  source ${HOME}/.bashrc
+  mv ${_pkgs[0]} "${_conf}/"
+  sed -i "s/__USER__/$USER/" "${_conf}/${_pkgs[0]}/config"
+  mv "${_pkgs[6]}" "${_conf}/"
+  mv ${_wall} "${HOME}/.${_wall}"
+  mv ${_pkgs[1]} "${_conf}/"
+  chmod +x "${_conf}/${_pkgs[1]}/${_pkgs[1]}.sh"
+  mv ${_oth[0]} "${_lsf}/"
+  ${_oth[1]} -fv
+  source "${HOME}/.${_oth[2]}"
 }
 
 
